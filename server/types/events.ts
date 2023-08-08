b7657287e8fde5b2f3775bedba8fd597897f67ff
ivan <ivan.kavtarev@mail.ru>
2023-08-08 14:31:11 +0300

    fix

--- server/types/events.ts
+++ server/types/events.ts
@@ -1,2 +1,2 @@
-export type IEvents = 'join' | 'create' | 'leave' | 'message';
-export const allowedEvents: IEvents[] = ['join', 'create', 'leave', 'message'];
+export const allowedEvents = ['join', 'create', 'leave', 'message'] as const;
+export type IEvents = (typeof allowedEvents)[number];
